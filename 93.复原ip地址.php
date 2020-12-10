b803b6caf0e6c31801053e36cd8c1f745d2654bd
cxx <cxx2320@foxmail.com>
2020-12-10 10:59:29 +0800

    93

---
+++ 93.复原ip地址.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=93 lang=php
+ *
+ * [93] 复原IP地址
+ */
+
+// @lc code=start
+class Solution
+{
+
+    public $res = [];
+
+    public $s_length = 0;
+    /**
+     * @param String $s
+     * @return String[]
+     */
+    function restoreIpAddresses($s)
+    {
+        $this->s_length = strlen($s);
+        if ($this->s_length < 4) {
+            return [];
+        }
+        $this->find($s, 0, '', 0);
+        return $this->res;
+    }
+
+    /**
+     * 寻找ip
+     *
+     * @param string $s
+     * @param int $index
+     * @param string $ip
+     * @param int $start 从
+     * @return void
+     */
+    function find($s, $index, $ip, $start)
+    {
+        if ($index > 3 && $start === $this->s_length) {
+            $this->res[] = $ip;
+            return;
+        }
+        if ($index > 3) {
+            return;
+        }
+        $tmp = '';
+        for ($i = $start; $i < $this->s_length; $i++) {
+            $tmp .= $s[$i];
+            // 分别判断 长度大于3，数字大于255，有没有前导0的情况
+            if (strlen($tmp) > 3 || intval($tmp) > 255 || $tmp !== (string)intval($tmp)) {
+                return;
+            }
+            $this->find($s, $index + 1, ($ip !== '' ? $ip . '.' : '') . $tmp, $i + 1);
+        }
+    }
+}
+// @lc code=end
