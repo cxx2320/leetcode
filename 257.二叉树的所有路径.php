35b41391f453cf425baab4be5fc3451bdd3ebd06
cxx <cxx2320@foxmail.com>
2021-01-19 10:58:37 +0800

    257：两种方法实现

--- 257.二叉树的所有路径.php
+++ 257.二叉树的所有路径.php
@@ -18,6 +18,41 @@
 class Solution
 {
 
+    private $res = [];
+
+    /**
+     * @param TreeNode $root
+     * @return String[]
+     */
+    function binaryTreePaths($root)
+    {
+        $this->dfs($root, '');
+        return $this->res;
+    }
+
+    function dfs($root, $path)
+    {
+        if ($root === null) {
+            return;
+        }
+        $path = $path ? $path . '->' : '';
+        if ($root->left === null && $root->right === null) {
+            $this->res[] = $path . $root->val;
+            return;
+        }
+        if ($root->left !== null) {
+            $this->dfs($root->left, $path . $root->val);
+        }
+        if ($root->right !== null) {
+            $this->dfs($root->right, $path . $root->val);
+        }
+    }
+}
+// @lc code=end
+
+class Solution1
+{
+
     /**
      * @param TreeNode $root
      * @return String[]
@@ -34,7 +69,7 @@ class Solution
             return [];
         }
         if ($root->left === null && $root->right === null) {
-            return [$root->val];
+            return [(string)$root->val];
         }
         if ($root->left !== null) {
             $res1 = $this->dfs($root->left);
@@ -52,4 +87,3 @@ class Solution
         return $arr;
     }
 }
-// @lc code=end
