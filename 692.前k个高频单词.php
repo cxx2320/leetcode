9df55d3304c70f6c862821a9152bb88df4262ad7
cxx <cxx2320@foxmail.com>
2021-01-22 18:04:47 +0800

    692：最小堆

--- 692.前k个高频单词.php
+++ 692.前k个高频单词.php
@@ -6,7 +6,64 @@
  */
 
 // @lc code=start
+
+class MyminHeap extends SplMinHeap
+{
+    public function compare($a, $b)
+    {
+        if ($a[0] === $b[0]) {
+            if ($a[1] === $b[1]) {
+                return 0;
+            } else {
+                return $a[1] < $b[1] ? -1 : 1;
+            }
+        }
+        return $a[0] > $b[0] ? -1 : 1;
+    }
+}
+
 class Solution
+{
+
+    /**
+     * @param String[] $words
+     * @param Integer $k
+     * @return String[]
+     */
+    function topKFrequent($words, $k)
+    {
+        $counts = array_count_values($words);
+        $heap = new MyminHeap();
+        foreach ($counts as $word => $count) {
+            if ($heap->count() < $k) {
+                $heap->insert([$count, $word]);
+            } else {
+                $top_word = $heap->top();
+                if ($top_word[0] < $count) {
+                    $heap->extract();
+                    $heap->insert([$count, $word]);
+                }
+                if ($top_word[0] === $count && $word < $top_word[1]) {
+                    $heap->extract();
+                    $heap->insert([$count, $word]);
+                }
+            }
+        }
+        $res = array_fill(0, $heap->count() - 1, '');
+        while (!$heap->isEmpty()) {
+            [, $word] = $heap->extract();
+            $size = $heap->count();
+            $res[$size] = $word;
+        }
+        return $res;
+    }
+}
+// @lc code=end
+
+/**
+ * 使用系统函数实现
+ */
+class Solution1
 {
 
     /**
@@ -36,4 +93,3 @@ class Solution
         return array_splice(array_keys($map), 0, $k);
     }
 }
-// @lc code=end
