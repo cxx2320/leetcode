78d6451c4f4a021c2476bba1df71fe444efc5215
cxx2320 <2320142695@qq.com>
2021-09-22 15:55:17 +0800

    78：迭代方式

--- 78.子集.php
+++ 78.子集.php
@@ -6,7 +6,7 @@
  */
 
 // @lc code=start
-class Solution
+class Solution1
 {
 
     private $res = [];
@@ -28,9 +28,32 @@ class Solution
             $this->res[] = $arr;
             return;
         }
-        $this->dfs($nums, $length, $index + 1, $arr);// 不选当前$nums[$index]值
+        $this->dfs($nums, $length, $index + 1, $arr); // 不选当前$nums[$index]值
         array_push($arr, $nums[$index]);
-        $this->dfs($nums, $length, $index + 1, $arr);// 选择当前$nums[$index]值
+        $this->dfs($nums, $length, $index + 1, $arr); // 选择当前$nums[$index]值
+    }
+}
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function subsets($nums)
+    {
+        $res = [[]];
+        for ($i = 0; $i < count($nums); $i++) {
+            // 这里必须重新计算数目
+            $count = count($res);
+            for ($j = 0; $j < $count; $j++) {
+                $res[] = array_merge($res[$j], $nums[$i]);
+            }
+        }
+        return $res;
     }
 }
 // @lc code=end
+
+$s = new Solution();
+var_dump($s->subsets([1, 2, 3]));
